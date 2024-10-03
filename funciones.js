24ab6324a51f466ad16d39dbde612c691082439b
Fabian Andres Ovalle Reyes <fabianmadara8@gmail.com>
2024-10-02 20:55:12 -0500

    hice cambio en Style.css agregue js en login y solucione error en estilo css en login

---
+++ funciones.js
@@ -0,0 +1,7 @@
+function validaRegistro(){
+    let usuario=document.getElementById("usuario").value;
+    console.log(usuario);
+    let contrasena=document.getElementById("contrasena").value;
+    console.log(contrasena);
+    window.location.href="Pagina-principal.hml";
+}

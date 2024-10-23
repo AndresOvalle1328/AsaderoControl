446284ac080f1edae5f92cbd75eccfd08aff3814
Fabian Andres Ovalle Reyes <fabianmadara8@gmail.com>
2024-10-22 21:00:45 -0500

    ciclos

--- javascrip/pagina-principal.js
+++ javascrip/pagina-principal.js
@@ -4,7 +4,7 @@ function agregarProducto() {
     let cantidad = document.getElementById("cantidad").value;
     let fecha_Ingreso = document.getElementById("fechaIngreso").value;
     let fecha_Vencimiento = document.getElementById("fechaVencimiento").value;
-    
+
 
     if (nombre == "" || categoria == "" || cantidad == "" || fecha_Vencimiento == "" || fecha_Ingreso == "") {
         alert("Ningún campo puede estar vacío");
@@ -34,7 +34,7 @@ function agregarProducto() {
         document.getElementById("cantidad").value = "";
         document.getElementById("fechaIngreso").value = "";
         document.getElementById("fechaVencimiento").value = "";
-        
+
     }
 }
 
@@ -56,3 +56,112 @@ function eliminarFila(boton) {
     fila.remove();
 }
 
+let entradas = [
+    {
+        "username": "bode2024",
+        "producto": "pollo",
+        "categoria": "carnicos",
+        "fecha": "03-10-2024",
+        "fechaVencimiento": "03-11-2024",
+        "cantidadIngreso": "50",
+        "total": "50"
+    },
+    {
+        "username":"bode2024",
+        "producto": "pollo Broster",
+        "categoria": "Carnicos",
+        "fecha": "04-10-2024",
+        "fechaVencimiento": "04-11-2024",
+        "cantidadIngreso": 45,
+        "Total": 45
+    },
+    {
+        "username":"bode2024",
+        "producto": "bandeja mixta",
+        "categoria": "Congelados",
+        "fecha": "02-11-2024",
+        "fechaVencimiento": "02-12-2024",
+        "cantidadIngreso": 15,
+        "Total": 15
+    },
+    {
+        "username":"bode2024",
+        "producto": "Carne",
+        "categoria": "Carnicos",
+        "fecha": "04-10-2024",
+        "fechaVencimiento": "04-12-2024",
+        "cantidadIngreso": 80,
+        "Total": 80
+    },
+    {
+        "username":"bode2024",
+        "producto": "churrasco",
+        "categoria": "Carnicos",
+        "fecha": "04-11-2024",
+        "fechaVencimiento": "30-10-2024",
+        "cantidadIngreso": 20,
+        "Total": 20
+    }
+];
+let salidas = [
+    {
+        "username":"bode2024",
+        "producto": "pollo",
+        "categoria": "Carnicos",
+        "personal":"Marcos",
+        "fecha": "03-10-2024",
+        "fechaVencimiento": "03-11-2024",
+        "cantidadSalida": 35,
+        "Total": 15
+    },
+    {
+        "username":"bode2024",
+        "producto": "pollo Broster",
+        "categoria": "Carnicos",
+        "personal":"laura",
+        "fecha": "04-10-2024",
+        "fechaVencimiento": "04-11-2024",
+        "cantidadSalida": 20,
+        "Total": 25
+    },
+    {
+        "username":"bode2024",
+        "producto": "bandeja mixta",
+        "categoria": "Congelados",
+        "personal":"luisa",
+        "fecha": "02-11-2024",
+        "fechaVencimiento": "02-12-2024",
+        "cantidadSalida": 10,
+        "Total": 5
+    },
+    {
+        "username":"bode2024",
+        "producto": "Carne",
+        "categoria": "Carnicos",
+        "personal":"angelica",
+        "fecha": "04-10-2024",
+        "fechaVencimiento": "04-12-2024",
+        "cantidadSalida": 65,
+        "Total": 10
+    },
+    {
+        "username":"bode2024",
+        "producto": "churrasco",
+        "categoria": "Carnicos",
+        "personal":"laura",
+        "fecha": "04-10-2024",
+        "fechaVencimiento": "30-10-2024",
+        "cantidadSalida": 8,
+        "Total": 12
+    }
+];
+
+// console.log(entradas[0].producto);
+// console.log(entradas[1].producto);
+entradas.forEach(function(recorridoInventario){
+console.log(recorridoInventario.username);
+});
+
+for(contador=0;contador<=2;contador++){
+    console.log(entradas[contador]);
+}
